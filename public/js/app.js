45553086700945598f770ab3911e538fc7a36e99
FrancescaLong <francesca_long@hotmail.com>
2015-10-29 09:57:50 -0700

    moved login js code to login.js from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -33,31 +33,6 @@ vegSearch.addEventListeners();
 
 
 
-/*  Signin and Signup information FROM BRIANNA*/
-
-  $('#signup-form').on('submit', function(event) {
-    event.preventDefault();
-    // select the form and serialize its data
-    var signupData = $("#signup-form").serialize();
-    console.log(signupData);
-    // send POST request to /users with the form data
-    $.post('/user-show', signupData, function(response) {
-      console.log(response);
-    });
-  });
-
-
-  $('#login-form').on('submit', function(e) {
-    e.preventDefault();
-    // select the form and serialize its data
-    // note: this is the form because the event handler
-    //   was triggered from the form
-    var loginData = $(this).serialize();
-    // send POST request to /login with the form data
-    $.post('/login', loginData, function(response) {
-      console.log(response);
-    });
-  });
 
 
 
@@ -134,5 +109,5 @@ veggieSearch.prototype.addEventListeners = function(){   //VeggieSearch.prototyp
   //       }
   //   });
   // });  
-//};
+//}; -- this is the dash to remove
 //});
--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,31 @@ console.log('sanity check: client-side js LOGIN.JS loaded');
 
 
 
+/*  Signin and Signup information FROM BRIANNA*/
+
+  $('#signup-form').on('submit', function(event) {
+    event.preventDefault();
+    // select the form and serialize its data
+    var signupData = $("#signup-form").serialize();
+    console.log(signupData);
+    // send POST request to /users with the form data
+    $.post('/user-show', signupData, function(response) {
+      console.log(response);
+    });
+  });
+
+
+  $('#login-form').on('submit', function(e) {
+    e.preventDefault();
+    // select the form and serialize its data
+    // note: this is the form because the event handler
+    //   was triggered from the form
+    var loginData = $(this).serialize();
+    // send POST request to /login with the form data
+    $.post('/login', loginData, function(response) {
+      console.log(response);
+    });
+  });
 
 
 });  //  CLOSING TAGS
